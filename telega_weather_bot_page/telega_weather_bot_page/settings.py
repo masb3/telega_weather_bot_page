7578815e0b8de79e8b8c9179f32d1bda6d2d3b25
dima <amid.vonavi@gmail.com>
2020-04-02 00:41:53 +0300

    fixing db deployment

--- telega_weather_bot_page/telega_weather_bot_page/settings.py
+++ telega_weather_bot_page/telega_weather_bot_page/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import django_heroku
+import dj_database_url
 
 
 TELEGA_WEATHER_BOT_PAGE_DB = os.getenv("TELEGA_WEATHER_BOT_PAGE_DB")
@@ -92,6 +93,9 @@ DATABASES = {
         'PORT': '',
     }
 }
+
+prod_db = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
 
 
 # Password validation
